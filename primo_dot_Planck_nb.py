d4bd0fc0ca1b70b50a972e87792131786d7d0397
zbw2525 <128143419+zbw2525@users.noreply.github.com>
2023-05-09 13:33:57 +0100

    Update primo_dot_Planck_nb.py

--- primo_dot_Planck_nb.py
+++ primo_dot_Planck_nb.py
@@ -19,17 +19,19 @@ line_remove = 9 #from which line to read the file n-ijk
 dk = kmax-kmin
 K = kmax+kmin
 log = True
-remove_bad = True #substitute the supplementary mode in planck basis with simple fourier mode
+remove_bad = False #substitute the supplementary mode in planck basis with simple fourier mode.
+
+atol = 1.49e-8
+rtol = 1.49e-8
+t_max = 360 #maximum time to calculate one element.
+array_file_name = 'omega_n_m_501_680.txt' # name of the txt file of the restored omega array
 
 #integral variable transiformation
 #x to kbar
 @nb.njit
 def kbar(x):
     return 2*x*kmax/dk -K/dk
-#x to xbar = (k-kmin)/(kmax-kmin), which is the argument of the fourier modes in planck primodal basis with 1d domain (0,1)
-@nb.njit
-def xbar(x):
-    return x*kmax/dk-kmin/dk
+
 #kbar to k
 @nb.njit
 def unbar(kbar, kmin = kmin, kmax = kmax):
@@ -51,9 +53,9 @@ def one_d_basis_f(i, x, pmax = pmax_pl):
             m = np.mod(i,2)
             n = i+m
             if m == 0:
-                result = np.cos(np.pi*n*xbar(x))
+                result = np.cos(np.pi*n*x)
             else:
-                result = np.sin(np.pi*n*xbar(x))
+                result = np.sin(np.pi*n*x)
     return result
 
 #generate 3d planck basis, then symmetrize.
@@ -205,16 +207,25 @@ def m_ijk(pmax):
                     m+=1
     return np.array(mijk_list)
 
+#weight function, used to determine the dot product between basis functions over tetrapyd. w = 1/(k1+k2+k3)
+@nb.njit
+def w(x,y,z, kmax = kmax):
+    if (x > 0.001)*(y > 0.001)*(z > 0.001):
+        w = 1/kmax*1/(x+y+z)
+    else:
+        w = 0
+    return w
+
 #numerical integrator
 def Gamma_n_m_nb(n_i, n_j, n_k, m_i, m_j, m_k, kmax, atol = 1.49e-8, rtol = 1.49e-8): #internal use, n is the index of 3d planck basis, m is the index of 3d primodal basis
     
     #doing the integration over tetrapyed (excluding the small k slices)
     @nb.njit
     def f1(z,x,y):
-        return three_d_basis_f_eval(n_i,n_j,n_k, x, y, z)*pri_basis(m_i, kbar(x))*pri_basis(m_j, kbar(y))*pri_basis(m_k, kbar(z))
+        return w(x,y,z)*three_d_basis_f_eval(n_i,n_j,n_k, x, y, z)*pri_basis(m_i, kbar(x))*pri_basis(m_j, kbar(y))*pri_basis(m_k, kbar(z))
     @nb.njit    
     def f2(z,y,x):
-        return three_d_basis_f_eval(n_i,n_j,n_k, x, y, z)*pri_basis(m_i, kbar(x))*pri_basis(m_j, kbar(y))*pri_basis(m_k, kbar(z))
+        return w(x,y,z)*three_d_basis_f_eval(n_i,n_j,n_k, x, y, z)*pri_basis(m_i, kbar(x))*pri_basis(m_j, kbar(y))*pri_basis(m_k, kbar(z))
     
     I1, E1 = tplquad(f1, 0, 0.5, lambda y: y, lambda y: 1-y, lambda y, x: x-y, lambda x, y: x+y, epsabs=atol, epsrel=rtol) #in the limit lambda, the sequence of
     I2, E2 = tplquad(f1, 0.5, 1, lambda y: 1-y, lambda y: y, lambda y, x: y-x, 1, epsabs=atol, epsrel=rtol)                # y and x matters!
@@ -314,17 +325,133 @@ def Gamma_array_MIP(): #m: primodal, n: planck
 
     return Gamma_n_m_array, Gamma_n_ijk_array
 
+#use master slave algorithm to avoid wasting calculating efficiency caused by non-uniform distributions of work load for each rank
+def omega_array_ms(atol, rtol, time_max):
+    #initialize mpi
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+
+    #get the map between n, m to ijk
+    nijk_array = nijk_map(planck_file, line_remove)
+    mijk_array = m_ijk(pmax)
+    #flatten NxM array to an 1-d array, and get the map between i to n-m
+    omega_index_array = np.zeros((len(nijk_array), len(mijk_array),2))
+    for n in range(len(nijk_array)):
+        for m in range(len(mijk_array)):
+            omega_index_array[n][m][0] = n
+            omega_index_array[n][m][1] = m
+    #1d map between the task index i and omega array index n,m.
+    map_i_nm = omega_index_array.reshape(len(nijk_array)*len(mijk_array),2)
+
+    #master rank (rank = 0): allocate tasks and integrate result.
+    if rank==0:
+        #set the empty array to restore the calculation result of omega matrix element
+        omega_array = np.zeros((len(nijk_array), len(mijk_array)))
+        
+         # define list of tasks
+        tasklist = (i for i in range(len(map_i_nm)))
+        #set count of finished ranks
+        finished = 0
+        #create status object for MPI_probe
+        status = MPI.Status()
+
+        for i in range(1,size):
+        # check there are enough jobs for the number of ranks
+        # -1 is a flag to say "no more work" 
+            try:
+                message = next(tasklist)
+            except StopIteration:
+                message = -1
+        
+            # now we send initial job ID's to slaves
+            print("[{}] Sending task: {} to rank {}".format(rank,message,i))
+            comm.isend(message, dest=i, tag=i)
+
+        # Now we check for messages from complete jobs then allocate new jobs to the slaves
+        while finished<size-1:
+            # Check for waiting messages with probe 
+            flag = comm.iprobe(status=status, source=MPI.ANY_SOURCE, tag=MPI.ANY_TAG)
+        
+            # if a message is waiting then enter this loop to recieve it
+            if flag==True:
+                # status object stores the origin of the message (and tag etc..)
+                source = status.source
+                #recv the message
+                test = comm.irecv(source=source, tag=source)
+                reply = test.wait()
+                #print("[{}] Recieving result: {} from rank {}".format(rank,reply,source))
+            
+                # now check the reply, -1 means the slave receieved a -1 and it's letting
+                # us know that it's finished so we add it to our finshed count
+                # otherwise we send it its next job ID (which may be -1 if there are 
+                # no more jobs)
+                if reply[0]==-1:
+                    finished +=1
+                    print("[{}] Recieved termination finished count: {}".format(rank,finished))
+                else:
+                    #print("[{}] Done with result {}".format(rank,reply))
+                    n = int(map_i_nm[int(reply[1])][0])
+                    m = int(map_i_nm[int(reply[1])][1])
+                    omega_array[n][m] = reply[0]
+                    try:
+                        message = next(tasklist)
+                    except StopIteration:
+                        message = -1
+                    print("[{}] Sending task: {} to rank {}".format(rank,message,source))
+                    comm.isend(message, dest=source, tag=source)
+        
+    # Slave ranks section
+    else:
+        omega_array = np.zeros((len(nijk_array), len(mijk_array)))
+        # this loop keeps us checking for jobs until we recieve a -1
+        while True:
+            # recvout next job ID
+            test = comm.irecv(source=0, tag=rank)
+            task = test.wait()#task: job ID
+            print("[{}] Recieving task: {} from rank {}".format(rank,task,0))
+        
+            # is job ID = -1 then no more jobs and we can stop
+            # We let the master know we are stopping by sending -1 back
+            # Otherwise we do the job associated with the ID we recieve
+            if task==-1:
+                comm.isend(np.array([-1]), dest=0, tag=rank)
+                print("[{}] Sending termination to rank {}".format(rank,0))
+                break
+            else:
+                n = int(map_i_nm[task][0])
+                m = int(map_i_nm[task][1]) 
+
+                n_i = int(nijk_array[n][1])
+                n_j = int(nijk_array[n][2])
+                n_k = int(nijk_array[n][3])
+                m_i = int(mijk_array[m][1])
+                m_j = int(mijk_array[m][2])
+                m_k = int(mijk_array[m][3])
+                # These lines are the actual job
+                try:
+                    result = func_timeout(time_max, Gamma_n_m_nb, args=(n_i, n_j, n_k, m_i, m_j, m_k, kmax, atol, rtol))
+
+                except FunctionTimedOut:
+                    result = 999
+                
+                # now we send our result back to the master
+                comm.isend(np.array([result,task]), dest=0, tag=rank)
+                #print("[{}] Sending result {} to rank {}".format(rank,result,0))
+
+    return omega_array
+
 #main
 t1 = time()
-Gamma_n_m_array, Gamma_n_ijk_array = Gamma_array_MIP()
+#omega_n_m_array, omega_n_ijk_array = Gamma_array_MIP()
+omega_n_m_array = omega_array_ms(atol, rtol, t_max)
 t2 = time()
 
-#np.savetxt('gamma_n_m_5_10', Gamma_n_m_array)
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
-if rank == 0: #Gamma_n_m_array != None:
-    print(Gamma_n_m_array)
-    print(Gamma_n_ijk_array)
-    np.savetxt('gamma_n_m_500_680.txt', Gamma_n_m_array)
+if rank == 0: 
+    print(omega_n_m_array)
+    #print(omega_n_ijk_array)
+    np.savetxt(array_file_name, omega_n_m_array)
 
 print("time consuming "+str(t2-t1))
